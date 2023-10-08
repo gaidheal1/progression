1a91209173a2ace8518b69e93d9d286e66013f28
Duncan <rebelringers@rebellion.earth>
2023-10-08 20:35:27 +0100

    amy made me do it

--- js/scriptnpcs.js
+++ js/scriptnpcs.js
@@ -7,15 +7,17 @@ document.addEventListener("DOMContentLoaded",
       $ajaxUtils
         .sendGetRequest("/data/npc.json",
           function (res) {
-            var npc = res.npcs.npc100;
+            // var npc = res.npcs[0];
+            // console.log(npc.id);
+            var message = "";
+            var npc;
+            // message += "<p>" + npc.prefix + " " + npc.name + " is a " + npc.role + " in " + npc.location + "</p>";
+            for (var i = 0; i < res.length; i++) {
+              npc = res[i]
+              message += "<p>" + npc.prefix + " " + npc.name + " is a " + npc.role + " in " + npc.location + "</p>";
+            }
+            console.log(res[0].id);
             
-            var message = ""
-            
-            // for (npc in res.npcs) {
-            //   message += "<p>" + npc.prefix + " " + npc.name + " is a " + npc.role + " in " + npc.location + "</p>";
-            // }
-
-            message += "<p>" + npc.prefix + " " + npc.name + " is a " + npc.role + " in " + npc.location + "</p>";
             // message += "<p>Level " + npc.level + "</p>";
             // message += "<p>Experience: " + npc.xp + "/" + npc.reqXP + "</p>";
             // message += "<p>Health: " + npc.health + "/" + npc.maxHealth + "</p>";
--- js/scriptquests.js
+++ js/scriptquests.js
@@ -1,65 +1,125 @@
-// DOM Manipulation
-
+// List quests
 document.addEventListener("DOMContentLoaded", 
-	function (event) {
+  function (event) {
+  		console.log(event)
+        // Call server to get quest info
+      $ajaxUtils
+        .sendGetRequest("/data/quests.json",
+          function (res) {
+            
+            var message = "<h2>Quest list</h2>";
+            var quest, link;
+            for (var i = 0; i < res.length; i++) {
+            	quest = res[i];
+            	link = "quest2.html?id=" + quest.id;
+              message += "<p>&quot;" + quest.name + "&quot;: " + quest.description + "</p>";
+              message += "<p>Repeatable: " + quest.repeat + ".</p>";	
+              message += "<p><button id='" + quest.id + "'><a href='" + link + "'>Start quest</a></button></p>";
 
-	// Unobtrusive event binding
-	document.querySelector("#step2")
-		.addEventListener("click", function () {
-			var self = this;
-			
-
-			// Call server to get quest info
-			$ajaxUtils
-				.sendGetRequest("data/quests.json",
-					function (res) {
-						var message = 
-							res.quests.quest03.questText.step3
-						console.log(res.quests)
-					
-
-						document.querySelector("#step2Content")
-							.innerHTML = message;
-					});
-				
-	});
+            };
+            
+            document.querySelector("#content")
+              .innerHTML = message; 
+          });
+  //   // Unobtrusive event binding
+	// document.querySelector("button")
+	// 	.addEventListener("click", function () {
+	// 		var self = this;
+	// 		console.log(this.id);
+	// 	});    
 });
+// console.log("test");
+
+// Load requested quest
 
 // document.querySelector("button")
-// 	.onclick = sayHello;
+// 	.addEventListener("click", 
+// 		function (event) {
 
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
+// 	// Unobtrusive event binding
+// 	// document.querySelector("button")
+// 	// 	.addEventListener("click", function () {
+// 			var self = this;
+// 			console.log(this.id);
+			
+// 			// Call server to get quest info
+// 			$ajaxUtils
+// 				.sendGetRequest("/data/quests.json",
+// 					function (res) {
+// 						var message = 
+// 							res.quests.quest03.questText.step3
+// 						console.log(res.quests)
+					
 
-    function xpCalc (event) {
-      console.log(event);
+// 						document.querySelector("#content")
+// 							.innerHTML = message;
+// 					});
+				
+// 	// });
+// });
 
-      // this.textContent = "Sent!";
-      var minutes =
-        Number(document.getElementById("minutes").value);
-      var choreBonus = minutes * .5;
+// // DOM Manipulation
+
+// document.addEventListener("DOMContentLoaded", 
+// 	function (event) {
+
+// 	// Unobtrusive event binding
+// 	document.querySelector("#step2")
+// 		.addEventListener("click", function () {
+// 			var self = this;
+			
+
+// 			// Call server to get quest info
+// 			$ajaxUtils
+// 				.sendGetRequest("data/quests.json",
+// 					function (res) {
+// 						var message = 
+// 							res[this.id].quest03.questText.step3
+// 						console.log(res.quests)
+					
+
+// 						document.querySelector("#step2Content")
+// 							.innerHTML = message;
+// 					});
+				
+// 	});
+// });
+
+// // document.querySelector("button")
+// // 	.onclick = sayHello;
+
+// // Event handling
+// document.addEventListener("DOMContentLoaded",
+//   function (event) {
+
+//     function xpCalc (event) {
+//       console.log(event);
+
+//       // this.textContent = "Sent!";
+//       var minutes =
+//         Number(document.getElementById("minutes").value);
+//       var choreBonus = minutes * .5;
       
-      var total = minutes + choreBonus;
+//       var total = minutes + choreBonus;
 
-      var message = "<h3>Player rewards:</h3>";
-      message += "<ul><li>Player experience: " + minutes + "xp</li>";
-      message += "<li>Chore bonus: " + choreBonus + "xp</li>";
-      message += "<li>Quest step complete: 10xp</li></ul>";
-      message += "<p>Player gets " + total + "xp!</p>";
-      message += "<h3>Character rewards:</h3>";
-      message += "<ul><li>Character experience: " + minutes + "xp</li>";
-      message += "<li>Stamina: -5</li></ul>";
+//       var message = "<h3>Player rewards:</h3>";
+//       message += "<ul><li>Player experience: " + minutes + "xp</li>";
+//       message += "<li>Chore bonus: " + choreBonus + "xp</li>";
+//       message += "<li>Quest step complete: 10xp</li></ul>";
+//       message += "<p>Player gets " + total + "xp!</p>";
+//       message += "<h3>Character rewards:</h3>";
+//       message += "<ul><li>Character experience: " + minutes + "xp</li>";
+//       message += "<li>Stamina: -5</li></ul>";
         
-      document
-        .getElementById("rewards")
-        .innerHTML = message;
-    }
+//       document
+//         .getElementById("rewards")
+//         .innerHTML = message;
+//     }
 
-  // Unobtrusive event binding
-  document.querySelector("#forRewards")
-    .addEventListener("click", xpCalc);
+//   // Unobtrusive event binding
+//   document.querySelector("#forRewards")
+//     .addEventListener("click", xpCalc);
 
-  }
-);
+//   }
+// );
 
